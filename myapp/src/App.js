8eee59cdcf7ab2d27dda6b76f2b1006173a6d143
lovkesh07 <79805511+lovkesh07@users.noreply.github.com>
2023-09-30 22:15:45 +0530

    sfd

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from './components/Footer';
+import { Routes ,Route } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
+import ScrollToTop from './components/ScrollToTop';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavMenu />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/about" element={<About />} />
+
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/projects" element={<Projects />} />
+
+          <Route path="/" element={<Home/>} />
+
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
-
-export default App;
